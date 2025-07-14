190e894c35f055a4bc5e3d2a70893863a696994f
Kajal <kajalola48@gmail.com>
2025-07-14 08:04:48 +0530

    Create 7_numsandmath.js

---
+++ .devcontainer/1_basics/7_numsandmath.js
@@ -0,0 +1,25 @@
+const score = 400
+// console.log(score);
+
+const balance =  new Number(100)
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toFixed(1));
+const otherNumber = 23.845
+// console.log(otherNumber.toPrecision(2));
+
+const hundereds = 100000
+// console.log(hundereds.toLocaleString('en-IN'));
+
+// MATH
+
+console.log(Math.abs(4));
+console.log(Math.abs(-4))
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+const min = 10 
+const max = 50
+
+console.log(Math.floor(Math.random()*(max-min+1)+min))
