991a4071e5d06a0e9bfd40713cc4b5b85ce74fd2
Kajal <kajalola48@gmail.com>
2025-07-15 12:13:01 +0530

    arrays

---
+++ .devcontainer/2_basics/1_array.js
@@ -0,0 +1,24 @@
+//ARRAYS
+const myArr = [0,1,2,3,4,5]
+
+console.log(myArr[2]);
+
+const myHeroes = ["shaktiman","batman"]
+
+const myArr2 = new Array (1,5,6,7)
+// ARRAY METHODS
+
+myArr.push(6)
+myArr.pop()//pop removes the last element
+myArr.unshift(9)
+myArr.shift()
+// console.log(myArr)
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(4));
+
+// SLICE
+
+console.log("a",myArr);
+const my1 = myArr.slice(1,3)
+console.log(my1)
+console.log("b",myArr);
