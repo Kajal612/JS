93839db7e4df1c7aa7050ed61ae67887e27acdc7
Kajal <kajalola48@gmail.com>
2025-07-21 15:28:05 +0530

    Create 2_switch.js

---
+++ .devcontainer/4_controlflow/2_switch.js
@@ -0,0 +1,14 @@
+const month  = 2
+
+switch(month){
+    case 1:
+    console.log("january");
+    break;
+    case 2:
+        console.log("feb");
+        break;
+
+        default:
+            console.log("default")
+            break;
+}
