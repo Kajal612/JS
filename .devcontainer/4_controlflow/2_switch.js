56caecb15382ff10febd4baf8e876ec8b1e92a5a
Kajal <kajalola48@gmail.com>
2025-07-22 07:44:49 +0530

    Update 2_switch.js

--- .devcontainer/4_controlflow/2_switch.js
+++ .devcontainer/4_controlflow/2_switch.js
@@ -12,3 +12,34 @@ switch(month){
             console.log("default")
             break;
 }
+
+
+//TRUTHY VALUES
+const useremail = "hitesh@gmail.com"
+
+if(useremail){
+    console.log("got user email")
+} else{
+    console.log("don not have user email")
+}
+
+
+// falsy values== false,0,-0,bigint 0n,"",null,undefined,NAN
+//truthy values=="0",'false'," ",[],{},function(){}
+
+if (useremail.length === 0){
+    console.log("array is empty");
+}
+
+const emptyobj = {}
+
+if (Object.keys(emptyobj).length === 0){
+    console.log("object is empty");
+}
+
+// terniary operator
+// condition? true : false
+
+const iceteaprice = 100
+iceteaprice <= 80 ? console.log("less than 80") : console.log
+("more than 80")
