a1e34eeb0f3b251f522b9c1a454712749aab4b10
Kajal <kajalola48@gmail.com>
2025-07-22 08:06:52 +0530

    loops basics

--- .devcontainer/5_loops/1_basics.js
+++ .devcontainer/5_loops/1_basics.js
@@ -7,7 +7,37 @@ for(let index = 0;index <10; index++){
 for (let i=0; i <=10;i++) {
     console.log(`outer loop value:${i}`);
     for(let j = 0;j<= 10; j++){
-        console.log(`inner loop value${j} and inner loop ${i}`);
+    //     console.log(`inner loop value${j} and inner loop ${i}`);
+    // console.log(i + '*' + j + ' = ' + i*j);
     }
     
+}
+
+let myarray = ["flash", "batman", "superman"]
+// console.log(myarray.length);
+for (let index = 0;index < myarray.length;index++){
+    const element = myarray[index];
+    // console.log(element);
+}
+
+
+//BREAK AND CONTINUE 
+
+
+for (let index = 1;index <= 20;index++){
+    if (index == 5){
+        // console.log(`detected 5`)
+        break  //kisi bhi control flow ko break karna ho to hum break likhte h
+    }
+    //  console.log(`value of i is ${index}`);
+}
+
+
+
+for (let index = 1;index <= 20;index++){
+    if (index == 5){
+        console.log(`detected 5`)
+        continue
+    }
+     console.log(`value of i is ${index}`);
 }
